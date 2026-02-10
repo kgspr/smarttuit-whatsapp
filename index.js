27eb45cfe19446e1dfec6220d701242c9fb82285
KGS Pramuditha <pramudithakgs@gmail.com>
2026-02-10 14:15:08 +0530

    u

--- index.js
+++ index.js
@@ -219,17 +219,17 @@ app.post('/wa', authenticateBearer, async (req, res) => {
                     body: '{"account":51}'
                 };
 
-                await fetch('https://lms.eu1.storap.com/files/d248ab87-902a-42f1-8eeb-e3a317a5262d', options)
-                .then((resImg) => {
-                    return res
-                    .status(200)
-                    .json(resImg)
-                })
-                    .catch(err => {
-                        return res
-                            .status(200)
-                            .json(withHome("Something went wrong!"))
-                    });
+                // await fetch('https://lms.eu1.storap.com/files/d248ab87-902a-42f1-8eeb-e3a317a5262d', options)
+                // .then((resImg) => {
+                //     return res
+                //     .status(200)
+                //     .json(resImg)
+                // })
+                //     .catch(err => {
+                //         return res
+                //             .status(200)
+                //             .json(withHome("Something went wrong!"))
+                //     });
 
                 return res
                     .status(200)
